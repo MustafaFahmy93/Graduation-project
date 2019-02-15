de31e64ec21158920388db09aad925b03f085049
Mustafa Fahmy <mustafa_fahmy2010@yahoo.com>
2019-02-15 23:06:17 +0200

    New ctrl

--- Car_python/New_ctrl/ctrl_small(needForSpeed_ver2).py
+++ Car_python/New_ctrl/ctrl_small(needForSpeed_ver2).py
@@ -18,7 +18,6 @@ def camThread():
     count_file.close()
     for i in range(10000):
         cam(action,count)
-        print(count)
         count+=1
         if(action=="end"):
             end_cam()
@@ -33,20 +32,24 @@ def listen_key_Thread():
             dc=50
             forward(dc)
             action="1 0 0 0"
+            print("forward")
         elif(key == keyboard.Key.down or key == keyboard.KeyCode(char='s')):
             dc=50
             backward(dc)
             action="0 0 0 1"
+            print("backward")
         elif((key == keyboard.Key.right or key == keyboard.KeyCode(char='d'))and flag_up==0):
             flag_right=1
             dc=50
             right(dc)
             action="0 1 0 0"
+            print("right")
         elif((key == keyboard.Key.left or key == keyboard.KeyCode(char='a'))and flag_up==0):
             flag_left=1
             dc=50
             left(dc)
             action="0 0 1 0"
+            print("left")
         elif(key == keyboard.KeyCode(char='h')):
             if(dc!=100):
                 dc=(dc+10)%110
@@ -66,29 +69,19 @@ def listen_key_Thread():
             end_GPIO()
             exit(0)
 #================================(up_right & up_left)======================
-        if(key == keyboard.Key.right and flag_up==1):
+        if((key == keyboard.Key.right and flag_up==1)or(key == keyboard.Key.up and flag_right==1)):
             dc=60
             forward(dc)
             right(dc)
             action="0 1 0 0"
-        elif(key == keyboard.Key.left and flag_up==1):
+            print("F right")
+        elif((key == keyboard.Key.left and flag_up==1)or(key == keyboard.Key.up and flag_left==1)):
             dc=60
             forward(dc)
             left(dc)
             action="0 0 1 0"
-        elif(key == keyboard.Key.up and flag_right==1):
-            dc=60
-            forward(dc)
-            right(dc)
-            action="0 1 0 0"
-        elif(key == keyboard.Key.up and flag_left==1):
-            dc=60
-            forward(dc)
-            left(dc)
-            action="0 0 1 0"
-
-
-
+            print("F left")
+        
     def on_release(key):
         global flag_up,flag_right,flag_left
         if(key == keyboard.Key.up or key == keyboard.KeyCode(char='w')):
