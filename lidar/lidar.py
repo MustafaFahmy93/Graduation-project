91273497307a56852f593a242f82355c253e6d38
Mustafa Fahmy <33979158+MustafaFahmy93@users.noreply.github.com>
2019-07-07 15:19:39 +0200

    Update lidar.py

--- lidar/lidar.py
+++ lidar/lidar.py
@@ -1,10 +1,12 @@
 from rplidar import RPLidar
+import time
+# function to map the pixels of the object to the angle of the object
 
-#function to map the pixels of the object to the angle of the object
+
 def mapping_pix2angle(min_pixel, max_pixel):
 
-    #width of the capture images = 1280 pixel
-    #angle of the camera = 60
+    # width of the capture images = 1280 pixel
+    # angle of the camera = 60
     min_angle = min_pixel*60/1280
     print(min_angle)
     max_angle = max_pixel*60/1280
@@ -19,14 +21,14 @@ def object_depth_try(min, max):
 
     center_angle = mapping_pix2angle(min, max)
 
-    #wide the range of the center angle
+    # wide the range of the center angle
 
     min_angle = center_angle - 2
     max_angle = center_angle + 2
 
     try:
-        #the address the lidar connected to COM5
-        lidar = RPLidar('COM9')
+        # the address the lidar connected to COM5
+        lidar = RPLidar('/dev/ttyUSB0')
 
         angleList = []
         distanceList = []
@@ -36,7 +38,7 @@ def object_depth_try(min, max):
         for scan in lidar.iter_scans():
             # for loop to take the required range of the camera from 0 to 60
             for (_, angle, distance) in scan:
-                if (angle >= 0 and angle <= 30) or (angle>=330 and angle<=360):
+                if (angle >= 0 and angle <= 30) or (angle >= 330 and angle <= 360):
                     if (angle >= 0 and angle <= 30):
                         angle = angle + 30
                         angleList.append(angle)
@@ -48,21 +50,19 @@ def object_depth_try(min, max):
             i += 1
             if i > 3:
                 break
-        #anglelist contain all the angles
-        #distancelist contain distance at each angle
+        # anglelist contain all the angles
+        # distancelist contain distance at each angle
 
         lidar.stop()
         lidar.stop_motor()
         lidar.disconnect()
 
-
-
-         #for loop to take distances of the object
+        # for loop to take distances of the object
 
         for i in range(len(angleList)):
-            if angleList[i]>=min_angle and angleList[i]<=max_angle:
+            if angleList[i] >= min_angle and angleList[i] <= max_angle:
                 objectDis.append(distanceList[i])
-        #the average of the distances
+        # the average of the distances
 
         objectAvgDistance = sum(objectDis)/len(objectDis)
 
@@ -72,31 +72,28 @@ def object_depth_try(min, max):
         return None
 
 
-def object_depth(min,max) :
+def object_depth(min, max):
 
-    x = object_depth_try(min,max)
+    x = object_depth_try(min, max)
     while(x == None):
         print('error ')
-        x = object_depth_try(min,max)
+        x = object_depth_try(min, max)
     return x
 
 
-
-
-
-
+start = time.clock()
 x = object_depth(240, 600)
+end = time.clock()
 print(x)
+print(end-start)
 
-
-
-    # print(sum(objectAvgDis)/len(objectAvgDis))
-    #
-    # print(angleList)
-    # print(distanceList)
-    # print(len(angleList))
-    # print(len(distanceList))
-    #
-    # lidar.stop()
-    # lidar.stop_motor()
-    # lidar.disconnect()
+# print(sum(objectAvgDis)/len(objectAvgDis))
+#
+# print(angleList)
+# print(distanceList)
+# print(len(angleList))
+# print(len(distanceList))
+#
+# lidar.stop()
+# lidar.stop_motor()
+# lidar.disconnect()
